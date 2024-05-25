7b12782cfe3f6950e934e9e20805117561af9895
Jacek Kosinski <kashiash@gmail.com>
2024-05-25 15:14:36 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Jacek Kosinski U on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
